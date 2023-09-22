9f6e2713ebdfbce6b2142137f8bf86f03abf2bd1
AntonPnv <anton.panov992@gmail.com>
2023-09-22 14:53:34 +0400

    Account.js completed

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,16 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
+  static URL = '/account'; // Переопределяет статичное свойство URL
+
   /**
    * Получает информацию о счёте
    * */
   static get(id = '', callback){
-
+    createRequest({
+      url: this.URL + '/' + id,
+      method: 'GET',
+      callback,
+    });
   }
 }
